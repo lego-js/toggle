83ca47db99e189d44e83d8c1aeb3c0c9c9f29939
Brandon Poe <brandon.poe@union.co>
2016-03-21 11:24:44 -0400

    ie fix

--- src/utils/state.js
+++ src/utils/state.js
@@ -1,53 +1,60 @@
 function hasClassInList( elem, c ) {
-	return elem.classList.contains( c );
+    return elem.classList.contains( c );
 }
 
 function addClassInList( elem, c ) {
-	elem.classList.add( c );
+    elem.classList.add( c );
 }
 
 function removeClassInList( elem, c ) {
-	elem.classList.remove( c );
+    elem.classList.remove( c );
 }
 
 function hasClassInName( elem, c ) {
-	return ` ${ elem.className } `.indexOf( ` ${ c } ` ) > -1;
+    return ` ${ elem.className } `.indexOf( ` ${ c } ` ) > -1;
 }
 
 function addClassInName( elem, c ) {
-	if ( !hasClassInName( elem, c ) ) {
-		elem.className = `${elem.className} ${c}`;
-	}
+    if ( !hasClassInName( elem, c ) ) {
+        elem.className = `${elem.className} ${c}`;
+    }
 }
 
 function removeClassInName( elem, c ) {
-	elem.className = ` ${ elem.className } `.replace( new RegExp( ` ${ c } `, 'g' ), ' ' ).trim();
+    elem.className = ` ${ elem.className } `.replace( new RegExp( ` ${ c } `, 'g' ), ' ' ).trim();
 }
 
-const LIST_SUPPORT = 'classList' in document.documentElement;
-
 const classie = {
-	has: LIST_SUPPORT ? hasClassInList : hasClassInName,
-	add: LIST_SUPPORT ? addClassInList : addClassInName,
-	remove: LIST_SUPPORT ? removeClassInList : removeClassInName,
-	toggle: function ( elem, c ) {
-		if ( !this.has( elem, c ) ) {
-			this.add( elem, c );
-		}
-		else {
-			this.remove( elem, c );
-		}
-	}
+    has: function ( elem, c ) {
+        if ( !elem || !c ) return false;
+        return 'classList' in elem ? hasClassInList(elem, c) : hasClassInName(elem, c);
+    },
+    add: function ( elem, c ) {
+        if ( !elem || !c ) return;
+        return 'classList' in elem ? addClassInList(elem, c) : addClassInName(elem, c);
+    },
+    remove: function ( elem, c ) {
+        if ( !elem || !c ) return;
+        return 'classList' in elem ? removeClassInList(elem, c) : removeClassInName(elem, c);
+        },
+    toggle: function ( elem, c ) {
+        if ( !this.has( elem, c ) ) {
+            this.add( elem, c );
+        }
+        else {
+            this.remove( elem, c );
+        }
+    }
 };
 
 export default function state(elem, className, active) {
 
-	if (typeof active === 'undefined') {
-		return classie.has(elem, `is-${className}`);
-	}
+    if (typeof active === 'undefined') {
+        return classie.has(elem, `is-${className}`);
+    }
 
-	var service = active === 'toggle' ? 'toggle' : active ? 'add' : 'remove';
+    var service = active === 'toggle' ? 'toggle' : active ? 'add' : 'remove';
 
-	classie[service](elem, `is-${className}`);
-	return !!active;
+    classie[service](elem, `is-${className}`);
+    return !!active;
 }
