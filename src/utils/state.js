93ec2a9d4ac58b483183f00ad7b01519454dac40
Brandon Poe <brandon.poe@union.co>
2016-04-19 23:31:35 -0400

    version bump. state util revised

--- src/utils/state.js
+++ src/utils/state.js
@@ -4,10 +4,12 @@ function hasClassInList( node, c ) {
 
 function addClassInList( node, c ) {
     node.classList.add( c );
+    return true;
 }
 
 function removeClassInList( node, c ) {
     node.classList.remove( c );
+    return false;
 }
 
 function hasClassInName( node, c ) {
@@ -19,14 +21,16 @@ function addClassInName( node, c ) {
         let className = `${node.className} ${c}`;
         node.className.baseVal ? node.className.baseVal = className : node.className = className;
     }
+    return true;
 }
 
 function removeClassInName( node, c ) {
     let className = ` ${ node.className } `.replace( new RegExp( ` ${ c } `, 'g' ), ' ' ).trim();
     node.className.baseVal ? node.className.baseVal = className : node.className = className;
+    return false;
 }
 
-const classie = {
+const service = {
     has: function ( node, c ) {
         if ( !node || !c ) return false;
         return 'classList' in node ? hasClassInList(node, c) : hasClassInName(node, c);
@@ -38,25 +42,20 @@ const classie = {
     remove: function ( node, c ) {
         if ( !node || !c ) return;
         return 'classList' in node ? removeClassInList(node, c) : removeClassInName(node, c);
-        },
+    },
     toggle: function ( node, c ) {
-        if ( !this.has( node, c ) ) {
-            this.add( node, c );
-        }
-        else {
-            this.remove( node, c );
-        }
+        return this[ !this.has( node, c ) ? 'add' : 'remove' ]( node, c );
+    },
+    test: function ( node, c, active ) {
+        return this[ !!active ? 'add' : 'remove' ]( node, c );
     }
 };
 
-export default function state(node, className, active) {
+export default function (node, className, active) {
 
-    if (typeof active === 'undefined') {
-        return classie.has(node, `is-${className}`);
+    if ( typeof active === 'undefined' ) {
+        return service.has( node, `is-${className}` );
     }
 
-    var service = active === 'toggle' ? 'toggle' : active ? 'add' : 'remove';
-
-    classie[service](node, `is-${className}`);
-    return !!active;
+    return service[active === 'toggle' ? 'toggle' : 'test']( node, `is-${className}`, active );
 }
